26c0871d68392d995dcb09fddac9b0f09e7d65ab
ianq1w1 <ianpereiraian2@gmail.com>
2026-08-04 01:45:49 -0300

    dependencies

---
+++ qualfoi/src/main/java/com/app/qualfoi/QualfoiApplication.java
@@ -0,0 +1,13 @@
+package com.app.qualfoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualfoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualfoiApplication.class, args);
+	}
+
+}
